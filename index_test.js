2cf5e20ac1faafdcb1e1a1db23c37b825aa0c28a
Tianna Spears <140460149+tspizzlee@users.noreply.github.com>
2024-01-31 11:49:41 +0000

    added documentation to README.md, fixed error in index.js, wrote test suite for index_test.js, and added comments to make code more readable in index_test.js

--- index_test.js
+++ index_test.js
@@ -1,1 +1,76 @@
 // test suite goes here
+
+// Step 1 and 2
+const assert= require('assert');
+const Rooster= require('../index');
+
+// Step 3 
+describe('Rooster', () => {
+// Step 4 (first method)
+  describe('.announceDawn', () => {
+// Step 5   
+    it ('returns a rooster call', () => {
+
+// Step 6-- Four Phases of a Test
+  
+  // Setup
+  const expected = 'cock-a-doodle-doo!';
+
+  // Exercise
+  const actual= Rooster.announceDawn();
+
+  // Verify
+  assert.equal(actual, expected);
+
+  // Teardown (optional)
+  
+        });
+    });
+});
+
+
+// Step 8 (second method)
+describe('.timeAtDawn', () => {
+  it('returns its argument as a string', () => {
+
+// Four Phases of a Test
+
+  // Setup
+  const testNumber = 12; // testing hour by using inputNumber 
+  const expected = '12';
+  
+  // Exercise
+  const actual = Rooster.timeAtDawn(testNumber);
+
+  // Verify
+  assert.equal(actual, expected);
+});
+});
+
+
+it('throws a range error if passed a number less than 0',  () => {
+
+  // Setup
+  const testNumber = -1;
+  const expected= RangeError; 
+
+  // Verify & Exercise-- included in verify to make code more efficient and clear
+
+  assert.throws(() => {
+    Rooster.timeAtDawn(testNumber); // Exercise
+      }, expected);
+
+  });
+
+it('throws a range error if passed a number greater than 23', () => {
+
+  // Setup
+  const testNumber = 24;
+  const expected = RangeError;
+
+  // Verify & Exercise-- included in verify to make code more efficient and clear
+
+    assert.throws(() => {
+      Rooster.timeAtDawn(testNumber)
+        }, expected)
+    });
